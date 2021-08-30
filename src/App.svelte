e1bf1717b66db427692b2d4b5ce85bab65c203ad
Luis Steidle <luis@steidle.me>
2021-08-30 16:17:15 +0200

    added meta tags

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,40 @@
   import Header from "./components/header.svelte";
   import Slider from "./components/slider.svelte";
   import Animation from "./components/animation.svelte";
+  import MetaTags from "svelte-meta-tags";
   export let speed;
 </script>
 
 <svelte:head>
   <title>Respire - Breathing App</title>
 </svelte:head>
+
+<MetaTags
+  title="Respire - Breathing app"
+  description="Respire is a basic tool for breathing exercises. Simply adjust your breath to the circle."
+  canonical="https://www.respire.site/"
+  openGraph={{
+    url: "https://www.respire.site/",
+    title: "Respire - Breathing app",
+    description:
+      "Respire is a basic tool for breathing exercises. Simply adjust your breath to the circle.",
+    images: [
+      {
+        url: "./img/preview-01.png",
+        width: 800,
+        height: 600,
+        alt: "Respire App",
+      },
+      {
+        url: "./img/preview-02.png",
+        width: 900,
+        height: 800,
+        alt: "Respire App",
+      },
+    ],
+    site_name: "Respire App",
+  }}
+/>
 
 <main>
   <Header />
