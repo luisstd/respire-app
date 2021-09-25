6fe5e7ea380f908888908b83c42f98f52da475f9
Luis Steidle <luis@steidle.me>
2021-09-25 21:47:19 +0200

    added new meta tags

--- src/App.svelte
+++ src/App.svelte
@@ -9,16 +9,28 @@
 <svelte:head>
   <!-- Primary Meta Tags -->
   <title>Respire - Breathing App</title>
-  <meta name="title" content="Respire - Breathing app" />
+  <meta name="title" content="Respire - Breathing App" />
   <meta
     name="description"
     content="Respire is a basic tool for breathing exercises. Simply adjust your breath to the circle."
   />
 
+  <!-- Open Graph Tags -->
+  <meta property="og:title" content="Respire - Breathing App" />
+  <meta
+    property="og:description"
+    content="Respire is a basic tool for breathing exercises. Simply adjust your breath to the circle."
+  />
+  <meta
+    property="og:image"
+    content="https://www.respire.site/img/preview.png"
+  />
+  <meta property="og:url" content="http://www.respire.site" />
+
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://www.respire.site/" />
-  <meta property="og:title" content="Respire - Breathing app" />
+  <meta property="og:title" content="Respire - Breathing App" />
   <meta
     property="og:description"
     content="Respire is a basic tool for breathing exercises. Simply adjust your breath to the circle."
@@ -26,11 +38,12 @@
 
   <!-- Twitter -->
   <meta property="twitter:url" content="https://www.respire.site/" />
-  <meta property="twitter:title" content="Respire - Breathing app" />
+  <meta property="twitter:title" content="Respire - Breathing App" />
   <meta
     property="twitter:description"
     content="Respire is a basic tool for breathing exercises. Simply adjust your breath to the circle."
   />
+  <meta name="twitter:card" content="summary_large_image" />
 
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
